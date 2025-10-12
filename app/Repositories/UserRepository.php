8cf8b0364f95f142258dfa5de8022f7dc3fd77a0
asadi20 <asadi20@gmail.com>
2025-10-12 17:54:30 +0330

    feat(repository): Implement UserRepository for user data persistence
    Adds UserRepository implementing UserRepositoryInterface.
    Includes methods for fetching users by ID and username, and inserting a new user record.
    Uses PDO directly and fetches results as UserModel instances via FETCH_CLASS.

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,81 @@
+<?php
+namespace app\Repositories;
+
+use app\Models\UserModel;
+use app\Repositories\UserRepositoryInterface;
+use PDO;
+
+class UserRepository implements UserRepositoryInterface
+{
+    private PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function findByUserId(int $id): ?UserModel
+    {
+        try {
+            $sql = 'SELECT * FROM users WHERE id= :id';
+            $stmt = $this->db->prepare($sql);
+            $stmt->setFetchMode(PDO::FETCH_CLASS, UserModel::class);
+            $stmt->bindValue('id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $user = $stmt->fetch();
+            return $user ?: null;
+        } catch (\PDOException $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * for find user data by username
+     * @param $username
+     * @return UserModel|null
+     */
+    public function findByUsername(string $username): ?UserModel
+    {
+        try {
+            $sql = 'SELECT * FROM users WHERE user_name= :user_name';
+            $stmt = $this->db->prepare($sql);
+            $stmt->setFetchMode(PDO::FETCH_CLASS, UserModel::class);
+            $stmt->bindValue('user_name', $username, PDO::PARAM_STR);
+            $stmt->execute();
+            $user = $stmt->fetch();
+            return $user ?: null;
+        } catch (\PDOException $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * for register a new user by user or by admin
+     * @param $userData
+     * @return string|null of last Insert ID or false
+     */
+    public function addNewUser(array $userData): string|null
+    {
+        try {
+            $sql = 'INSERT INTO users (user_name, full_name, password, email) VALUES (:user_name, :full_name, :password, :email)';
+
+            $stmt = $this->db->prepare($sql);
+
+            $stmt->bindValue('user_name', $userData['user_name'], PDO::PARAM_STR);
+            $stmt->bindValue('full_name', $userData['full_name'], PDO::PARAM_STR);
+            $stmt->bindValue('password', $userData['password'], PDO::PARAM_STR);
+            $stmt->bindValue('email', $userData['email'], PDO::PARAM_STR);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                // PDO::lastInserId does not return integer format of Id of the last inserted row jsut retrun string !!
+                // PDO::laseInsertId (int|false)
+                return $this->db->lastInsertId();
+            } else {
+                return null;
+            }
+        } catch (\PDOException $e) {
+            throw $e;
+        }
+    }
+}
