46f1e008217dcc8447e56e92c8567359e9f89351
asadi20 <asadi20@gmail.com>
2025-11-05 18:21:23 +0330

    fix(Response): Allow object input for Response::json method

--- core/Response.php
+++ core/Response.php
@@ -4,7 +4,13 @@ namespace Core;
 
 class Response
 {
-    public static function json(array $data, int $statusCode = 200): void
+    /**
+     * Convert output to json format
+     * @param array|object $data
+     * @param int $statusCode
+     * @return never
+     */
+    public static function json(array|object $data, int $statusCode = 200): void
     {
         header('Content-Type: application/json');
         http_response_code($statusCode);
@@ -12,6 +18,12 @@ class Response
         exit;
     }
 
+    /**
+     * convert output to text format
+     * @param string $text
+     * @param int $statusCode
+     * @return never
+     */
     public static function text(string $text, int $statusCode = 200): void
     {
         header('Content-Type: text/plain');
