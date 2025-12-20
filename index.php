1679ba375f3acc0f07be2d871d756b7f4b9fceef
asadi20 <asadi20@gmail.com>
2025-12-20 20:54:57 +0330

    feat(api): add CORS headers and OPTIONS preflight handling
    - Add CORS headers to allow requests from http://localhost:3000
    - Support POST, GET, OPTIONS methods and required headers
    - Handle preflight OPTIONS requests with 200 response
    - Enable credentials for authenticated requests

--- index.php
+++ index.php
@@ -4,6 +4,16 @@ ini_set('display_errors', 1);
 use core\Router;
 use core\Container;
 
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 spl_autoload_register(function ($className) {
     // fix linux addressing system
     $className = str_replace('\\', '/', $className);
