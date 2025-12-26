11056fd171f20036319d794beb5fc7b5561e3c4a
asadi20 <asadi20@gmail.com>
2025-12-26 22:47:41 +0330

    feat(rbacRepositoryInterface): add methods for permission lookup
    - Add findPermissionByUserId to check specific permission for user
    - Add findPermissionsByRoleId to retrieve permissions assigned to a role
    - Include comments for clarity on permission-related methods

--- app/Repositories/RbacRepositoryInterface.php
+++ app/Repositories/RbacRepositoryInterface.php
@@ -5,7 +5,10 @@ namespace app\Repositories;
 Interface RbacRepositoryInterface
 {
     public function findRolesByUserId(int $userId): ?array;
+    // find Permissions
     public function findPermissionsByUserId(int $userId): ?array;
+    // find Permission
+    public function findPermissionByUserId(int $userId, string $permissionName);
+    public function findPermissionsByRoleId(int $roleId);
     public function assignRolesToUser(int $userId, array $rolesId): ?int;
-    public function assignRolesToPermissions(array $roleId, array $permissionsId): ?int;
 }
